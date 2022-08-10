8486b10f1ec5668f3891c716fe67cbad96f8adab
Guppy-ZEN <39458650+g-zen@users.noreply.github.com>
2022-08-10 16:02:12 +0900

    GameManagerに最低限必要な機能を実装
    ゲームクリア
    ゲームオーバー
    リトライ機能の実装
    Guppy.csはテスト用に使うためignore

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start()
+    {
+
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameClear()
     {
-        
+        Debug.Log("Game Clear");
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public void Retry()
+    {
+        Debug.Log("Retry");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
