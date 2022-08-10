1959dd8e067abeeaa67e73228e200b553c6ac8fc
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2022-08-11 00:47:02 +0900

    SubStageを作ろうの会

---
+++ Assets/Script/SubStage/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [Header("プレイヤーキャラクタ"), SerializeField] private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, player.transform.position.y,transform.position.z);
+    }
+}
